37c18be7d7d4e8bde54c3715d0dec36f095a3ae1
JaqueBRSilva <j.silvabroccolo@gmail.com>
2025-08-08 16:26:41 -0300

    feat: [server] add CourseController

---
+++ desafio-revvo-backend/api/controllers/CourseController.php
@@ -0,0 +1,73 @@
+<?php 
+    require_once './models/Course.php';
+    
+    class CourseController {
+        private $db;
+    
+        public function __construct($conn) {
+            $this->db = $conn;
+        }
+    
+        public function index() {
+            $course = new Course($this->db);
+            $stmt = $course->listAllCourses();
+            $courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            echo json_encode($courses);
+        }
+    
+        public function store() {
+            $data = json_decode(file_get_contents("php://input"));
+            if (!isset($data->title) || !isset($data->description)) {
+                http_response_code(400);
+                echo json_encode(["mensagem" => "Dados incompletos."]);
+                return;
+            }
+    
+            $course = new Course($this->db);
+            $course->title = $data->title;
+            $course->description = $data->description;
+    
+            if ($course->createCourse()) {
+                http_response_code(201);
+                echo json_encode(["mensagem" => "Curso criado com sucesso."]);
+            } else {
+                http_response_code(500);
+                echo json_encode(["mensagem" => "Erro ao criar o curso."]);
+            }
+        }
+
+        public function update($id) {
+            $data = json_decode(file_get_contents("php://input"));
+    
+            if (!isset($data->title) || !isset($data->description)) {
+                http_response_code(400);
+                echo json_encode(["mensagem" => "Dados incompletos."]);
+                return;
+            }
+    
+            $course = new Course($this->db);
+            $course->id = $id;
+            $course->title = $data->title;
+            $course->description = $data->description;
+    
+            if ($course->updateCourse()) {
+                echo json_encode(["mensagem" => "Curso atualizado com sucesso."]);
+            } else {
+                http_response_code(500);
+                echo json_encode(["mensagem" => "Erro ao atualizar o curso."]);
+            }
+        }
+
+        public function destroy($id) {
+            $course = new Course($this->db);
+            $course->id = $id;
+    
+            if ($course->deleteCourse()) {
+                echo json_encode(["mensagem" => "Curso deletado com sucesso."]);
+            } else {
+                http_response_code(500);
+                echo json_encode(["mensagem" => "Erro ao deletar o curso."]);
+            }
+        }
+    }    
+?>
