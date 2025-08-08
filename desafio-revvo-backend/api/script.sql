17f3171b3f9b8263c58a45623b25859a55620ac4
JaqueBRSilva <j.silvabroccolo@gmail.com>
2025-08-08 16:27:08 -0300

    feat: [server] incluído script sql

---
+++ desafio-revvo-backend/api/script.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS courses_revvo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
