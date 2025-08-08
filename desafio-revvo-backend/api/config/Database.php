8753208af7cc9844dcfa01867f0b23ab73c5725a
JaqueBRSilva <j.silvabroccolo@gmail.com>
2025-08-08 16:24:19 -0300

    feat: [server] add classe Database

---
+++ desafio-revvo-backend/api/config/Database.php
@@ -0,0 +1,24 @@
+<?php
+    class Database {
+        private $host = "localhost";
+        private $db_name = "courses_revvo";
+        private $username = "root";
+        private $password = "";
+        public $conn;
+
+        public function connect() {
+            $this->conn = null;
+            try {
+                $this->conn = new PDO(
+                    "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                    $this->username,
+                    $this->password
+                );
+                $this->conn->exec("set names utf8");
+            } catch(PDOException $e) {
+                echo "Erro na conexão: " . $e->getMessage();
+            }
+            return $this->conn;
+        }
+    }
+?>
