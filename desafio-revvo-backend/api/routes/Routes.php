d27c36ac246ee2024a1fca1aa42a9d9e9e406ba7
JaqueBRSilva <j.silvabroccolo@gmail.com>
2025-08-08 21:50:23 -0300

    refactor: [server] atualizado rotas em Routes

--- desafio-revvo-backend/api/routes/Routes.php
+++ desafio-revvo-backend/api/routes/Routes.php
@@ -11,13 +11,43 @@
     $uri = explode('/', trim($uri, '/'));
     
     if ($uri[0] === 'courses') {
-        if ($method === 'GET') {
-            $controller->index();
-        } elseif ($method === 'POST') {
-            $controller->store();
-        } else {
-            http_response_code(405);
-            echo json_encode(["mensagem" => "Método não permitido"]);
+    
+        $id = $uri[1] ?? null;
+
+        switch ($method) {
+            case 'GET':
+                if ($id) {
+                    $controller->show($id);
+                } else {
+                    $controller->index();
+                }
+                break;
+
+            case 'POST':
+                $controller->store();
+                break;
+
+            case 'PUT':
+                if ($id) {
+                    $controller->update($id);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(["mensagem" => "ID não informado."]);
+                }
+                break;
+
+            case 'DELETE':
+                if ($id) {
+                    $controller->destroy($id);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(["mensagem" => "ID não informado."]);
+                }
+                break;
+
+            default:
+                http_response_code(405);
+                echo json_encode(["mensagem" => "Método não permitido"]);
         }
     } else {
         http_response_code(404);
